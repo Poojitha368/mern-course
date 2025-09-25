a71b99dd1a98cc2e868d2a85444aa4ae42f9ed5b
Poojitha@368 <poojithareddy2662004@gmail.com>
2025-09-25 13:08:45 +0530

    unordered list

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-console.log("welcome to todo app");
+// console.log("welcome to todo app");
 
 
-todos = []
-let work = prompt("enter what you want task to do ? (add/list/delete)")
+// todos = []
+// let work = prompt("enter what you want task to do ? (add/list/delete)")
 
-while(true){
-    if (work == "quit"){
-        break;
-    }
-    else if(work=="add"){
-    let task = prompt("enter task to add");
-    todos.push(task);
-    console.log("task added");
-    }
-    else if(work=="list"){
-        for(let i=0;i<todos.length;i++){
-            console.log(i,todos[i]);
-        }
-        console.log("===========================")
-    }
-    else if(work == "delete"){
-        let idx = prompt("enter task idx to delete todo");
-        todos.splice(idx,1);
-        console.log("task deleted");
-    }
-    work = prompt("enter what you want task to do ? (add/list/delete)");
-}
+// while(true){
+//     if (work == "quit"){
+//         break;
+//     }
+//     else if(work=="add"){
+//     let task = prompt("enter task to add");
+//     todos.push(task);
+//     console.log("task added");
+//     }
+//     else if(work=="list"){
+//         for(let i=0;i<todos.length;i++){
+//             console.log(i,todos[i]);
+//         }
+//         console.log("===========================")
+//     }
+//     else if(work == "delete"){
+//         let idx = prompt("enter task idx to delete todo");
+//         todos.splice(idx,1);
+//         console.log("task deleted");
+//     }
+//     work = prompt("enter what you want task to do ? (add/list/delete)");
+// }
 
-console.log("quitting todo app");
+// console.log("quitting todo app");
